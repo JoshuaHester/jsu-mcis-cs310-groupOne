8fd04f8f79ac4e07794c2136f893110ebf61e9a1
sfedyk <sfedyk@jsu.edu>
2014-10-02 13:59:40 -0500

    Acceptance Test
    These are the acceptance test

---
+++ acceptance/GroupOneKeywords.java
@@ -0,0 +1,52 @@
+import edu.jsu.mcis.*;
+
+public class GroupOneKeywords(){
+	
+	private ArgumentValue p;
+	
+	public void startCalVolume(String s) {
+		p = new ArgumentValue();
+		p.addArgument("length");
+		p.addArgument("width");
+		p.addArgument("height");
+		p.parse(s);
+	}
+	
+	public String getLength(){
+		return p.getArgument("length").getValue();
+	}
+	
+	public String getWidth(){
+		return p.getArgument("width").getValue();
+	}
+	
+	public String getHeight(){
+		return p.getArgument("height").getValue();
+	}
+	
+	public void startGetPet(String s) {
+		p = new ArgumentValue();
+		p.addArgument("pet");
+		p.addArgument("number");
+		p.addArgument("rainy");
+		p.addArgument("bathrooms");
+		p.parse(s);
+	}
+	
+	public String getPet(){
+		return p.getArgument("pet").getValue();
+	}
+	
+	public String getNumber(){
+		return p.getArgument("number").getValue();
+	}
+	
+	public String getRainy(){
+		return p.getArgument("rainy").getValue();
+	}
+	
+	public String getBathrooms(){
+		return p.getArgument("bathrooms").getValue();
+	}
+	
+}
