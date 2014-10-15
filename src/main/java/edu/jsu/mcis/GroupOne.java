88ad9a3dd20050f9f9d2f311eaa19c55df52c50b
Joshua <joshua.a.hester@gmail.com>
2014-10-14 21:52:19 -0500

    Main update please read desc.
    we have a "expected float, found Object" issue with the actual output.
    not sure how to fix this other than by converting the output into a
    string and then back into a float in the main

--- src/main/java/edu/jsu/mcis/GroupOne.java
+++ src/main/java/edu/jsu/mcis/GroupOne.java
@@ -4,20 +4,30 @@ import java.util.*;
 import java.util.Scanner;
 
 public class GroupOne{
-
+	public static float CalVol(float l, float w, float h){
+		return l*w*h;
+	}
+	
 	public static void main(String args[]){
 		Scanner key = new Scanner(System.in);
 		String input = "";
 		while(!input.equals("QUIT")){
 			input = key.nextLine().trim();
 			ArgumentParser arg = new ArgumentParser();
-			arg.addArgument(ArgumentValues.Types.INT, "length");
-			arg.addArgument(ArgumentValues.Types.INT, "width");
-			arg.addArgument(ArgumentValues.Types.INT, "height");
+			arg.addArgument(ArgumentValues.Types.FLOAT, "length");
+			arg.addArgument(ArgumentValues.Types.FLOAT, "width");
+			arg.addArgument(ArgumentValues.Types.FLOAT, "height");
 			arg.parse(input);
 			if(arg.getHelpOut()){System.out.println(arg.getUsage());}
 			else if(arg.checkTooManyArg()){System.out.println(arg.missingArguments());}
 			else if(arg.checkTooFewArg()){System.out.println(arg.tooManyArguments());}
+			else{
+				float l = Float.parseFloat(arg.getArgument("length").getValue().toString());
+				float w = Float.parseFloat(arg.getArgument("width").getValue().toString());
+				float h = Float.parseFloat(arg.getArgument("height").getValue().toString());
+				float v = CalVol(l,w,h);
+				System.out.println(v);
+			}
 		}
 	}
 }
