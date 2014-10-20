e657d656da4a7b06ba60f0536ed97ba1d8ca5154
Joshua <joshua.a.hester@gmail.com>
2014-10-20 18:44:05 -0500

    main cleaned

--- src/main/java/edu/jsu/mcis/GroupOne.java
+++ src/main/java/edu/jsu/mcis/GroupOne.java
@@ -33,29 +33,4 @@ public class GroupOne{
 			}
 		}
 	}
-	
-/*	
-	public static void main(String args[]){
-		Scanner key = new Scanner(System.in);
-		String input = "";
-		while(!input.equals("QUIT")){
-			input = key.nextLine().trim();
-			ArgumentParser arg = new ArgumentParser();
-			arg.addArgument(ArgumentValues.Types.FLOAT, "length");
-			arg.addArgument(ArgumentValues.Types.FLOAT, "width");
-			arg.addArgument(ArgumentValues.Types.FLOAT, "height");
-			arg.parse(input);
-			if(arg.getHelpOut()){System.out.println(arg.getUsage());}
-			else if(arg.checkTooManyArg()){System.out.println(arg.tooManyArguments());}
-			else if(arg.checkTooFewArg()){System.out.println(arg.missingArguments());}
-			else{
-				float l = arg.getArgument("length").getValue();
-				float w = arg.getArgument("width").getValue();
-				float h = arg.getArgument("height").getValue();
-				float v = CalVol(l,w,h);
-				System.out.println(v);
-			}
-		}
-	}
-*/
 }
